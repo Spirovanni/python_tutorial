aa18bf141a041e4d29feca247c43493f1de3c2e0
Xavier Martinez <blackshieldsx@gmail.com>
2019-11-11 19:17:36 -0800

    Create Views

---
+++ demo/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+# Create your models here.
+class Book(models.Model):
+    # BOOKS = [
+    #     ('FRESHMAN', 'Freshman'),
+    #     ('SOPHOMORE', 'Sophomore'),
+    #     ('JUNIOR', 'Junior'),
+    #     ('SENIOR', 'Senior'),
+    # ]
+    title = models.CharField(
+        max_length=36,
+        null=True,
+        # unique=True,
+        # choices=BOOKS
+    )
+    description = models.TextField(max_length=256, blank=True, null=True, default=None)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=3)
