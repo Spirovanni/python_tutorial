ccf1951f876d0185d15d44ab767fc343190bdfee
Xavier Martinez <blackshieldsx@gmail.com>
2019-11-14 22:58:35 -0800

    20. Templates

--- demo/views.py
+++ demo/views.py
@@ -2,20 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 from .models import Book
-
+from django.shortcuts import render
 
 # Create your views here.
-class Another(View):
+# class Another(View):
+#
+#     book = Book.objects.get(id=4)
+#     output = f"We have {book.title} book with ID {book.id} in DB and it has a price of {book.price}<br>"
 
-    books = Book.objects.filter(is_published=True)
-    output = ''
+    # for book in books:
+    #     output += f"We have {book.title} book with ID {book.id} in DB and it has a price of {book.price}<br>"
 
-    for book in books:
-        output += f"We have {book.title} book with ID {book.id} in DB and it has a price of {book.price}<br>"
-
-    def get(self, request):
-        return HttpResponse(self.output)
-
+    # def get(self, request):
+    #     return HttpResponse(self.output)
 
 def first(request):
-    return HttpResponse('First message from views')
+    return render(request, 'first_temp.html')
